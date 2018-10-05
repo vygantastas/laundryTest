3226fda371d5f38ce5ddbdf275bfb97b94a8ef17
vygantastas <vygantastas@gmail.com>
2018-10-05 15:37:35 +0300

    Changed ClientContrpller

--- src/main/java/lt/bit/laundry/controllers/ClientController.java
+++ src/main/java/lt/bit/laundry/controllers/ClientController.java
@@ -41,34 +41,34 @@ public class ClientController {
 
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model) {
-        Note note = service.getNoteById(id);
+        Client note = service.getClientById(id);
         model.addAttribute("note", note);
         return "operations/edit";
     }
 
-    @PostMapping("/update")
-    public String saveNote(@RequestParam Integer id, @RequestParam String message,
-                           @RequestParam(value = "done", required = false) boolean done) {
-        service.updateNote(id, message, done);
-        return "redirect:/";
-    }
+//    @PostMapping("/update")
+//    public String saveNote(@RequestParam Integer id, @RequestParam String message,
+//                           @RequestParam(value = "done", required = false) boolean done) {
+//        service.updateNote(id, message, done);
+//        return "redirect:/";
+//    }
 
     @GetMapping("/new")
     public String newNote() {
         return "operations/new";
     }
 
-    @PostMapping("/save")
-    public String updateNote(@RequestParam String message) {
-        service.saveNote(new Note(message));
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id) {
-        service.deleteNote(id);
-        return "redirect:/";
-    }
+//    @PostMapping("/save")
+//    public String updateNote(@RequestParam String message) {
+//        service.saveClient(new Client(message));
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/delete/{id}")
+//    public String delete(@PathVariable Integer id) {
+//        service.deleteNote(id);
+//        return "redirect:/";
+//    }
 
     private List<Client> filterAndSort() {
         List<Client> notebook = null;
